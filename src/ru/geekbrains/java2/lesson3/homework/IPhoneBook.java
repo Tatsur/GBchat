a23df14cf1f12a4aafee0f7129fe4d406604469d
OKrylov <krylov92oa@gmail.com>
2020-10-02 00:48:27 +0300

    Lesson 3 - hw

---
+++ src/ru/geekbrains/java2/lesson3/homework/IPhoneBook.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.java2.lesson3.homework;
+
+import java.util.Set;
+
+public interface IPhoneBook {
+
+    void add(String surname, String phoneNumber);
+
+    Set<String> get(String surname);
+
+    Set<String> getAllSurnames();
+}
